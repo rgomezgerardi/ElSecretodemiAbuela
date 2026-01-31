2e187bb8941bce69a107aa5585299b652d5ede25
Ricardo Gomez <rgomezgerardi@gmail.com>
2026-01-31 15:07:11 -0300

    fix: MenuManager

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class LevelManager : MonoBehaviour
+public class MenuManager : MonoBehaviour
 {
     [SerializeField] private Button startButton;
     [SerializeField] private Button optionsButton;
