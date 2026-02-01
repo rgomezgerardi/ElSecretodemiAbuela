f3caeb0207be4d51bc19961d458125b8fd37a47a
Ricardo Gomez <rgomezgerardi@gmail.com>
2026-02-01 06:10:21 -0300

    feat: Menu Incompleto

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    public static PauseManager Instance;
+
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private InputActionReference pauseAction;
+
+    private bool isPaused;
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void OnEnable()
+    {
+        pauseAction.action.performed += OnPause;
+        pauseAction.action.Enable();
+    }
+
+    void OnDisable()
+    {
+        pauseAction.action.performed -= OnPause;
+        pauseAction.action.Disable();
+    }
+
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    private void OnPause(InputAction.CallbackContext ctx)
+    {
+        TogglePause();
+    }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+
+        Time.timeScale = isPaused ? 0f : 1f;
+        AudioListener.pause = isPaused;
+
+        pauseMenu.SetActive(isPaused);
+    }
+
+    public void ResumeGame()
+    {
+        if (!isPaused) return;
+
+        isPaused = false;
+        Time.timeScale = 1f;
+        AudioListener.pause = false;
+
+        pauseMenu.SetActive(false);
+    }
+
+    public void RestartLevel()
+    {
+        Time.timeScale = 1f;
+        AudioListener.pause = false;
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        AudioListener.pause = false;
+
+        SceneManager.LoadScene("MainMenu");
+    }
+}
